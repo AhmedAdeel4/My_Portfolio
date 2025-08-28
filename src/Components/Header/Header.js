861dfbfb8207b48e52d0f219598850357644d683
Ahmed Adeel4 <Ahmadalroby4@gmail.com>
2025-08-29 02:53:38 +0300

    media

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -9,11 +9,11 @@ export default function Header() {
         <a className='logo' href='/'> Ahmed <span className='Dot'>.</span> </a>
 
         <ul className={`nav ${open ? "open" : ""}`}>
-            <li><a href='#home'>Home</a></li>
-            <li><a href='#about'>About</a></li>
-            <li><a href='#skills'>Skills</a></li>
-            <li><a href='#projects'>Projects</a></li>
-            <li><a href='#contact'>Contact</a></li>
+            <li><a href='#home' onClick={() => setOpen(false)}>Home</a></li>
+            <li><a href='#about' onClick={() => setOpen(false)}>About</a></li>
+            <li><a href='#skills' onClick={() => setOpen(false)}>Skills</a></li>
+            <li><a href='#projects' onClick={() => setOpen(false)}>Projects</a></li>
+            <li><a href='#contact' onClick={() => setOpen(false)}>Contact</a></li>
         </ul>
 
         <div className='menu-icon' onClick={()=> setOpen(!open)}>☰</div>
